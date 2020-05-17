466d86c7e65c5c56efb826383cce7230da7085aa
Chansa CHISHIMBA <chishimb_c@qwasar.io>
2020-05-17 16:37:47 +0000

    my_range added

--- ex00/my_range.c
+++ ex00/my_range.c
@@ -2,23 +2,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-
-
 int* my_range(int param_1, int param_2) 
 {
-    int *retun_Value;
-    
-  
-    retun_Value = (int*) malloc(sizeof(int) * 5);
+    int *array;
+    int index = param_1;
+    int size = 0;
+        
+    while (index < param_2){
+            index += 1;
+            size += 1;
+    }
+    //size-->
+    array = malloc(sizeof(int) * size);
 
-    for (*retun_Value  = param_1; *retun_Value <= param_2; ++retun_Value)
-        if (param_1 >= param_2)
-            return NULL;
-        else if (param_2>param_1)
-    return retun_Value;
+    index = 0;
+    while(param_1 < param_2){
+        array[index] = param_1;
+        index += 1;
+        param_1 += 1;
+    }
+return array;
 }
-
 /*
 int main(){
     printf("The range of the minimum and maximum is %d\n",my_range(5, 9));
